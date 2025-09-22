e21967718b28cb14ad661e95d29a3cbff8317a8f
Tracey-goode <Jerand322@gmail.com>
2025-09-22 02:38:30 -0400

    seed_mjs

---
+++ seed.mjs
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import Book from './models/book.mjs';
+import Author from './models/author.mjs';
+import Genre from './models/genre.mjs';
